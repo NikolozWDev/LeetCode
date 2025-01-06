42bbe844cc5d802ef98c6fde442850bfd2d858a7
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-01-06 07:29:15 +0400

    finished all device (page 1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      'phone500': '500px',
+      'phone550': '550px',
+      'phone700': '700px',
+      'phone850': '850px',
+      'phone950': '950px',
+      'phone1000': '1000px',
+      'phone1200': '1200px',
+    },
   },
   plugins: [],
 }
