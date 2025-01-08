8fb8625ea211449899be24cb3b6d67720ca5f5f2
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-01-08 08:20:55 +0400

    finished all pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         'image10': "url('https://assets.leetcode.com/users/images/93ad444a-6425-413b-9094-8a613d7be5ab_1734667147.858678.png')",
         'image11': "url('https://assets.leetcode.com/users/images/49479bba-73b3-45d2-9272-99e773d784b2_1687290663.3168745.jpeg')",
         'image12': "url('https://assets.leetcode.com/users/images/770789b0-c96b-4663-86d1-baab25534864_1669795265.8012726.png')",
+        'image13': "url('https://support.leetcode.com/hc/theming_assets/01HZKM7BF3RJ24JXMS3608EC5Y')",
     },
     screens: {
       'phone500': '500px',
